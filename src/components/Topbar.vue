2755f353cc9e506c8680026f5c0b0bc5a26f29f3
RenatoAlves0 <renatoalves97.123@gmail.com>
2022-01-07 13:38:18 -0300

    TopBar responsivo

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,56 +1,55 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg navbar-light bg-light"
-    v-bind:class="{ navbarOpen: show }"
-  >
-    <a class="navbar-brand" href="#">Encurtador de URL</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Alterna navegação"
-      @click="toggleNavbar"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Encurtador de URL</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        @click="toggleNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div
-      class="collapse navbar-collapse"
-      id="navbarSupportedContent"
-      v-bind:class="{ in: show }"
-    >
-      <ul class="navbar-nav mr-auto col-6">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/nova_url"
-            >Adicionar URL</router-link
-          >
-        </li>
-      </ul>
-      <ul class="navbar-nav mr-auto col-6 topbar-right">
-        <li v-if="usuario" class="nav-item">
-          <a class="nav-link disabled">Olá {{ usuario.nome }} :-)</a>
-        </li>
-        <li v-else class="nav-item">
-          <a class="nav-link disabled">Olá Anônimo :-)</a>
-        </li>
-        <li v-if="!usuario" class="nav-item">
-          <router-link class="nav-link" to="/registrar"
-            >Registrar-se</router-link
-          >
-        </li>
-        <li v-if="!usuario" class="nav-item">
-          <router-link class="nav-link" to="/entrar">Entrar</router-link>
-        </li>
-        <li v-if="usuario" class="nav-item" @click="sair">
-          <a class="nav-link">Sair</a>
-        </li>
-      </ul>
+      <div
+        class="collapse navbar-collapse"
+        id="navbarSupportedContent"
+        :class="show ? '' : 'hidden'"
+      >
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/nova_url"
+              >Adicionar URL</router-link
+            >
+          </li>
+        </ul>
+        <ul class="d-flex navbar-nav">
+          <li v-if="usuario" class="nav-item">
+            <a class="nav-link disabled">Olá {{ usuario.nome }} :-)</a>
+          </li>
+          <li v-else class="nav-item">
+            <a class="nav-link disabled">Olá Anônimo :-)</a>
+          </li>
+          <li v-if="!usuario" class="nav-item">
+            <router-link class="nav-link" to="/registrar"
+              >Registrar-se</router-link
+            >
+          </li>
+          <li v-if="!usuario" class="nav-item">
+            <router-link class="nav-link" to="/entrar">Entrar</router-link>
+          </li>
+          <li v-if="usuario" class="nav-item" @click="sair">
+            <a class="nav-link">Sair</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -65,7 +64,7 @@ export default {
   },
   data() {
     return {
-      show: true,
+      show: false,
     };
   },
   methods: {
