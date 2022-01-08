4903a942167d478b01c1a6768cf5b18dad1b30ee
RenatoAlves0 <renatoalves97.123@gmail.com>
2022-01-07 21:28:08 -0300

    Configuração para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT || process.env.$PORT || 3001,
-  },
+  // server: {
+  //   port: process.env.PORT || process.env.$PORT || 3001,
+  // },
   plugins: [vue()],
 })
