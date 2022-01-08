1af8f53b229c6b5244d9843d3fb8f422b3279c47
RenatoAlves0 <renatoalves97.123@gmail.com>
2022-01-07 21:06:30 -0300

    Adicionando $PORT (permitir deploy)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || process.env.$PORT || 3001,
   },
   plugins: [vue()],
 })
